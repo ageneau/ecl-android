706cec76514b5b7d4bcdd807e384ad9a1591f21e
Sylvain Ageneau <ageneau@gmail.com>
2012-12-23 14:30:07 -0300

    Add mono's libgc from https://github.com/elijahtaylor/mono.git rev: 17d07f628c38368600796787d1756671574d18b2

---
+++ nacl/libgc/include/private/pthread_stop_world.h
@@ -0,0 +1,21 @@
+#ifndef GC_PTHREAD_STOP_WORLD_H
+#define GC_PTHREAD_STOP_WORLD_H
+
+struct thread_stop_info {
+    int	signal;
+    word last_stop_count;	/* GC_last_stop_count value when thread	*/
+    				/* last successfully handled a suspend	*/
+    				/* signal.				*/
+    ptr_t stack_ptr;  		/* Valid only when stopped.      	*/
+#ifdef NACL
+/* Grab NACL_GC_REG_STORAGE_SIZE pointers off the stack when going into */
+/* a syscall.  20 is more than we need, but it's an overestimate in case*/
+/* the instrumented function uses any callee saved registers, they may  */
+/* be pushed to the stack much earlier.  Also, on amd64 'push' puts 8   */
+/* bytes on the stack even though our pointers are 4 bytes.             */
+#define NACL_GC_REG_STORAGE_SIZE 20
+    ptr_t reg_storage[NACL_GC_REG_STORAGE_SIZE];
+#endif
+};
+    
+#endif
